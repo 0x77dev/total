c2fa93540437062191f22c56c293e9d0498a045a
Mikhail Marynenko <0x77dev@protonmail.com>
2020-07-18 16:40:50 +0300

    feat: 🎸 vuetify init

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+
 // @ts-ignore
 import { createProvider } from "./vue-apollo";
+
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+
   // @ts-ignore
   apolloProvider: createProvider(),
-  render: (h) => h(App),
+
+  vuetify,
+  render: (h) => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
-  </div>
+  <v-content>asd</v-content>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
 </script>
