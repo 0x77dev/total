fcd968afc59916905701c1191b9b7835a60b8063
Mikhail Marynenko <0x77dev@protonmail.com>
2020-07-18 16:26:56 +0300

    feat: 🎸 vue-apollo init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+// @ts-ignore
+import { createProvider } from "./vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  // @ts-ignore
+  apolloProvider: createProvider(),
+  render: (h) => h(App),
+}).$mount("#app");
