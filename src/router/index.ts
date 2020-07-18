7d869eed2d92d4cfd660a86b29db6d145eca2630
Mikhail Marynenko <0x77dev@protonmail.com>
2020-07-18 18:20:55 +0300

    perf: ⚡️ Router auth protection

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,22 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import firebase from "@/plugins/firebase";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    redirect: "/login",
+    redirect: "/home",
   },
   {
     path: "/home",
     name: "Home",
     component: Home,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
@@ -33,6 +37,15 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth && !firebase.auth().currentUser) {
+    next("login");
+  } else {
+    next();
+  }
 });
 
 export default router;
