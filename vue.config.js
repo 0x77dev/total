e09a513e7617e0b0f24287a1d6ef43e673353055
Mikhail Marynenko <0x77dev@protonmail.com>
2020-07-25 20:53:57 +0300

    style: 💄 Logo update

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
+  pwa: {
+    name: "Total",
+    themeColor: "#272727",
+    msTileColor: "#272727",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      display: "portrait",
+    },
+  },
 };
